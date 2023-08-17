b0dbcb247681e388bc91f3091eb35dbb90519f6d
Lars van Galen <hallo@webartisist.nlgalen.org>
2023-08-17 13:35:55 +0200

    feat: update page

--- src/Collections/AfilliateItem.php
+++ src/Collections/AfilliateItem.php
@@ -4,5 +4,14 @@ namespace Larsvg\StatamicAffiliate\Collections;
 
 class AfilliateItem
 {
+    public function __construct(
+        private string $productName,
+        private string $productDescription,
+        private float $deliveryCost,
+        private string $image,
+        private int $stock
+    ) {
+
+    }
 
 }
