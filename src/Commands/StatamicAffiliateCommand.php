bfffffc668edb5c237952043de301255afc19690
Lars van Galen <hallo@webartisist.nlgalen.org>
2023-10-26 11:33:20 +0200

    feat: implement awin feed

--- src/Commands/StatamicAffiliateCommand.php
+++ src/Commands/StatamicAffiliateCommand.php
@@ -2,27 +2,45 @@
 
 namespace Larsvg\StatamicAffiliate\Commands;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Utils;
 use Illuminate\Console\Command;
 use Larsvg\StatamicAffiliate\Collections\AffiliateCollection;
 use Larsvg\StatamicAffiliate\Collections\AfilliateItem;
 
 class StatamicAffiliateCommand extends Command
 {
-    public $signature = 'statamic-affiliate-import';
+    public $signature = 'app:statamic-affiliate-import';
 
     public $description = 'Import affiliate data';
 
     public function handle(): int
     {
-        $url = 'https://files.channable.com/NZsY2CW84UUY7_Qbmd7t9Q==.csv';
+        $this->comment('Importing affiliate data');
 
-        $items = new AffiliateCollection();
+        $client = new Client();
+        $response = $client->get(config('statamic-affiliate.awin_import_feed'));
 
-        $items[] = new AfilliateItem();
-        $items[] = new AfilliateItem();
-        $items[] = new AfilliateItem();
-        $items[] = new AfilliateItem();
-        $items[] = new AfilliateItem();
+        if ($response->getStatusCode() === 200) {
+            // Get the gzipped content and create a stream.
+            $stream = Utils::streamFor(gzdecode($response->getBody()));
+
+            // Now, you can read the CSV data from the stream.
+            $csvData = $stream->getContents();
+
+            dd($csvData);
+
+            // Process the CSV data as needed.
+        }
+
+
+//        $items = new AffiliateCollection();
+//
+//        $items[] = new AfilliateItem();
+//        $items[] = new AfilliateItem();
+//        $items[] = new AfilliateItem();
+//        $items[] = new AfilliateItem();
+//        $items[] = new AfilliateItem();
 
         $this->comment('All done');
 
