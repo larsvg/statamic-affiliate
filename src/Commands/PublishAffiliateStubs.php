074b3330973d649ff44ea50b31123660a03bb229
Lars van Galen <hallo@webartisist.nlgalen.org>
2024-04-18 10:42:36 +0200

    feat: publish stubs

--- src/Commands/PublishAffiliateStubs.php
+++ src/Commands/PublishAffiliateStubs.php
@@ -3,6 +3,8 @@
 namespace Larsvg\StatamicAffiliate\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Yaml\Yaml;
 
 class PublishAffiliateStubs extends Command
 {
@@ -11,7 +13,59 @@ class PublishAffiliateStubs extends Command
 
     public function handle()
     {
-        dd('Publish the affiliate stubs.');
+        $this->publishFieldsets();
+        $this->publishBlueprints();
+        $this->publishContent();
+        $this->publishViews();
+
+        $this->comment('All done');
+
+        return self::SUCCESS;
+    }
+
+    protected function publishBlueprints(): void
+    {
+        $blueprints = [
+            //
+        ];
+
+        foreach ($blueprints as $blueprint) {
+            File::copy(__DIR__ . '/../../stubs/blueprints/' . $blueprint, resource_path('blueprints/' . $blueprint));
+        }
+    }
+
+    protected function publishViews(): void
+    {
+        $views = [
+            //
+        ];
+
+        foreach ($views as $view) {
+            File::copy(__DIR__ . '/../../stubs/views/' . $view, resource_path('views/' . $view));
+        }
+    }
+
+    protected function publishFieldsets(): void
+    {
+        $fieldsets = [
+            //
+        ];
+
+        foreach ($fieldsets as $fieldset) {
+            File::copy(__DIR__ . '/../../stubs/fieldsets/' . $fieldset, resource_path('fieldsets/' . $fieldset));
+        }
+    }
+
+    protected function publishContent(): void
+    {
+        $contents = [
+            //
+        ];
+
+        foreach ($contents as $content) {
+            $file = 'content/' . $content;
+            File::copy(__DIR__ . '/../../stubs/content/' . $content, base_path($file));
+        }
     }
 
 }
