e3f5eb313a6f06ab42d767a67073bb53730596fa
Lars van Galen <hallo@webartisist.nlgalen.org>
2024-04-18 12:13:58 +0200

    feat: scaffold an exporter

--- src/Commands/MakeImporter.php
+++ src/Commands/MakeImporter.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\File;
 
 class MakeImporter extends Command
 {
-    protected $signature = 'affiliate:make:importer';
+    protected $signature   = 'affiliate:make:importer';
     protected $description = 'Create a new importer command';
 
     public function handle(): int
@@ -15,20 +15,27 @@ class MakeImporter extends Command
         $name = $this->ask('What is the name of the feed? (Only letters)');
         $name = preg_replace('/[^A-z]/', '', strtolower($name));
 
-        if(empty($name)) {
+        if (empty($name)) {
             $this->error('Invalid name');
             return self::FAILURE;
         }
 
-        $content = File::get(__DIR__ . '/../../stubs/commands/DemoAffiliateCommand.php.stub');
-        $content = str_replace('{feed-name-ucfirst}', ucfirst($name), $content);
-        $content = str_replace('{feed-name}', $name, $content);
-        $fileName = ucfirst($name).'AffiliateCommand.php';
-        File::put(app_path('console/Commands/') . $fileName, $content);
+        $content  = File::get(__DIR__ . '/../../stubs/commands/DemoAffiliateCommand.php.stub');
+        $content  = str_replace('{feed-name-ucfirst}', ucfirst($name), $content);
+        $content  = str_replace('{feed-name}', $name, $content);
+        $fileName = ucfirst($name) . 'AffiliateCommand.php';
+        $target   = app_path('console/Commands/') . $fileName;
+        if (File::exists($target)) {
+            $this->error('console/Commands/' . $fileName . ' already exists');
+            return self::FAILURE;
+        }
 
-        $this->info('Command created: ' . $fileName. '. Run `php artisan affiliate:'.$name.'-import` to import the feed.');
+        File::put($target, $content);
+
+        $this->info('Command created: ' . $fileName . '. Run `php artisan affiliate:' . $name . '-import` to import the feed.');
         $this->info('Add the command to the schedule in app/Console/Kernel.php');
 
         return self::SUCCESS;
     }
+
 }
