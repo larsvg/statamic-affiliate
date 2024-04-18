4e2e7015027c10c71e0a523d1001ad459734c3a2
Lars van Galen <hallo@webartisist.nlgalen.org>
2024-04-18 10:07:44 +0200

    feat: extend AddonServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Larsvg\StatamicAffiliate;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,7 @@
 namespace Larsvg\StatamicAffiliate\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Larsvg\StatamicAffiliate\StatamicAffiliateServiceProvider;
+use Larsvg\StatamicAffiliate\ServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -20,7 +20,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
-            StatamicAffiliateServiceProvider::class,
+            ServiceProvider::class,
         ];
     }
 
