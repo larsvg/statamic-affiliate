ac9e119c0dfb65b2fe0ce737c76b7b9640a7e3f2
Lars van Galen <hallo@webartisist.nlgalen.org>
2024-04-18 12:12:26 +0200

    feat: scaffold an exporter

--- src/Commands/MakeImporter.php
+++ src/Commands/MakeImporter.php
@@ -12,7 +12,7 @@ class MakeImporter extends Command
 
     public function handle(): int
     {
-        $name = $this->ask('What is the name of the importer? (Only letters)');
+        $name = $this->ask('What is the name of the feed? (Only letters)');
         $name = preg_replace('/[^A-z]/', '', strtolower($name));
 
         if(empty($name)) {
@@ -23,8 +23,11 @@ class MakeImporter extends Command
         $content = File::get(__DIR__ . '/../../stubs/commands/DemoAffiliateCommand.php.stub');
         $content = str_replace('{feed-name-ucfirst}', ucfirst($name), $content);
         $content = str_replace('{feed-name}', $name, $content);
+        $fileName = ucfirst($name).'AffiliateCommand.php';
+        File::put(app_path('console/Commands/') . $fileName, $content);
 
-        File::put(app_path('console/Commands/') . ucfirst($name).'AffiliateCommand.php', $content);
+        $this->info('Command created: ' . $fileName. '. Run `php artisan affiliate:'.$name.'-import` to import the feed.');
+        $this->info('Add the command to the schedule in app/Console/Kernel.php');
 
         return self::SUCCESS;
     }
