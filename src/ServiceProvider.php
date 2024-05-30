ffb98fc8df69b0e800421f49a41fdb3be8167d10
Lars van Galen <hallo@webartisist.nlgalen.org>
2024-05-30 09:27:45 +0200

    feat: category_url into package

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,7 @@ namespace Larsvg\StatamicAffiliate;
 
 use Larsvg\StatamicAffiliate\Commands\MakeImporter;
 use Larsvg\StatamicAffiliate\Commands\PublishAffiliateStubs;
+use Statamic\Facades\Collection;
 use Statamic\Providers\AddonServiceProvider;
 use Statamic\Statamic;
 
@@ -18,6 +19,11 @@ class ServiceProvider extends AddonServiceProvider
 
     public function boot()
     {
+        //https://statamic.dev/collections#using-fields-from-related-entries
+        Collection::computed('products', 'category_url', function ($entry, $value) {
+            return $entry->belongs_to?->url();
+        });
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 PublishAffiliateStubs::class,
